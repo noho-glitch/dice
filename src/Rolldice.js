1b6bb1be2f916c58e085d20fb702c21f7d9beef6
Dave Nolan <dpn.nolan@gmail.com>
2020-03-12 13:56:47 -0400

    animated dice while rolling

--- src/Rolldice.js
+++ src/Rolldice.js
@@ -42,8 +42,8 @@ class Rolldice extends Component {
     return (
       <div className="Rolldice">
         <div className="dice-container">
-          <Die face={this.state.die1} />
-          <Die face={this.state.die2} />
+          <Die face={this.state.die1} rolling={this.state.rolling}/>
+          <Die face={this.state.die2} rolling={this.state.rolling}/>
         </div>
         <button onClick={this.roll} disabled={this.state.rolling}>
             {this.state.rolling ? 'Rolling...' : 'Roll Dice!'}
