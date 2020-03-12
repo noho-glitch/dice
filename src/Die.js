b4ead3e7886dd26f7aae40e4ae41c27a3c0a241a
Dave Nolan <dpn.nolan@gmail.com>
2020-03-12 13:07:13 -0400

    dice rendering on page with props passed for face

---
+++ src/Die.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+import './Die.css'
+
+
+class Die extends Component {
+    render() {
+        return <i className={`fas fa-dice-${this.props.face}`}></i>
+    }
+}
+
+export default Die;
