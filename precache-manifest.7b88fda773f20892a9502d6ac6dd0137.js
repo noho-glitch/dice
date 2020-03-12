f243deb303f8703c58fbcaf23ec806a913bb9057
Dave Nolan <dpn.nolan@gmail.com>
2020-03-12 14:02:32 -0400

    Updates

---
+++ precache-manifest.7b88fda773f20892a9502d6ac6dd0137.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c072475ca8d773340b3b2084d0e040f1",
+    "url": "/dice/index.html"
+  },
+  {
+    "revision": "354a86263cfa9e5ff0c2",
+    "url": "/dice/static/css/main.4267eff7.chunk.css"
+  },
+  {
+    "revision": "015fb66b87caab21bc7c",
+    "url": "/dice/static/js/2.32859778.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/dice/static/js/2.32859778.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "354a86263cfa9e5ff0c2",
+    "url": "/dice/static/js/main.fed1a0a9.chunk.js"
+  },
+  {
+    "revision": "03507fbc34b78d8eddc6",
+    "url": "/dice/static/js/runtime-main.b8089ff5.js"
+  }
+]);
